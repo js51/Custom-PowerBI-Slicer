3b11d23edff26da2f9da89ff38e88e4abe9b5691
Joshua Stevenson <joshuas153@gmail.com>
2024-05-09 13:42:45 +1000

    remove unused settings

--- forcedSelectionSlicer/src/settings.ts
+++ forcedSelectionSlicer/src/settings.ts
@@ -35,30 +35,7 @@ import FormattingSettingsModel = formattingSettings.Model;
 /**
  * Data Point Formatting Card
  */
-class DataPointCardSettings extends FormattingSettingsCard {
-    defaultColor = new formattingSettings.ColorPicker({
-        name: "defaultColor",
-        displayName: "Default color",
-        value: { value: "" }
-    });
-
-    showAllDataPoints = new formattingSettings.ToggleSwitch({
-        name: "showAllDataPoints",
-        displayName: "Show all",
-        value: true
-    });
-
-    fill = new formattingSettings.ColorPicker({
-        name: "fill",
-        displayName: "Fill",
-        value: { value: "" }
-    });
-
-    fillRule = new formattingSettings.ColorPicker({
-        name: "fillRule",
-        displayName: "Color saturation",
-        value: { value: "" }
-    });
+class Settings extends FormattingSettingsCard {
 
     fontSize = new formattingSettings.NumUpDown({
         name: "fontSize",
@@ -68,7 +45,7 @@ class DataPointCardSettings extends FormattingSettingsCard {
 
     name: string = "dataPoint";
     displayName: string = "Data colors";
-    slices: Array<FormattingSettingsSlice> = [this.defaultColor, this.showAllDataPoints, this.fill, this.fillRule, this.fontSize];
+    slices: Array<FormattingSettingsSlice> = [this.fontSize];
 }
 
 /**
@@ -77,7 +54,7 @@ class DataPointCardSettings extends FormattingSettingsCard {
 */
 export class VisualFormattingSettingsModel extends FormattingSettingsModel {
     // Create formatting settings model formatting cards
-    dataPointCard = new DataPointCardSettings();
+    dataPointCard = new Settings();
 
     cards = [this.dataPointCard];
 }
